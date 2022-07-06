1cc24dd5adc15b1d21924d80cf0e3ae4ee5a5768
ixs-naoya-minamisono <92581356+ixs-naoya-minamisono@users.noreply.github.com>
2022-07-07 00:03:13 +0900

    first commit

---
+++ demo/credential.js
@@ -0,0 +1,7 @@
+/**
+ * fill in below and rename to credential.js
+ */
+export const Credentials = {
+  CLIENT_ID: "JqwDrc3-TEgbXiDnrrZnSlGvxpl8HouXs6RvYqauIdY",
+  CLIENT_SECRET: "sLuNCFpK9-npNIUsQ3l7I9c7LMUZ-S8YL7UYEJSKishpdU61szKHbamjGUHuWTIv9b3Vea43hpAfHth55O6Gsw",
+};
